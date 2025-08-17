5e12deb2fa15a85fee98f2b21b4d593c6e60e401
MeenaPriyanshi <thevinchi007@gmail.com>
2025-08-17 11:00:01 +0000

    delted unnecessary files and written functions to create and delte the flder

--- app.py
+++ app.py
@@ -1,27 +1,52 @@
 import subprocess
 import os
+import shutil
 
+# DOCKER THINGS
 IMAGE_NAME = "my-ubuntu-app"
 IMAGE_TAG = "latest"
 IMAGE = f"{IMAGE_NAME}:{IMAGE_TAG}"
 
-# Host directory
-host_path = os.getcwd()
+# TEST FOLDER
+TEST_FOLDER = "test"
 
-# --- Step 1: Check if image exists ---
-result = subprocess.run(
-    ["docker", "images", "-q", IMAGE],
-    capture_output=True, text=True
-)
+def create_folder_and_file(folder_name=TEST_FOLDER, file_name='main.py', content='print("\t=== This is the default one\\n\t=== Please change the code")'):
+    os.makedirs(folder_name, exist_ok=True)
+    with open(f'{folder_name}/{file_name}', 'w') as f:
+        f.write(content)
+    
+    
 
-if not result.stdout.strip():
-    print(f"[INFO] Image {IMAGE} not found. Building...")
-    subprocess.run(
-        ["docker", "build", "-t", IMAGE, "."],
-        check=True
-    )
-else:
-    print(f"[INFO] Image {IMAGE} already exists.")
+def delete_folder(folder_name=TEST_FOLDER):
+    if os.path.exists(folder_name):
+        shutil.rmtree(folder_name)
+
+
+# def create_image(IMAGE):
+#     # Host directory
+#     host_path = os.getcwd()
+
+#     # --- Step 1: Check if image exists ---
+#     result = subprocess.run(
+#         ["docker", "images", "-q", IMAGE],
+#         capture_output=True, text=True
+#     )
+
+#     if not result.stdout.strip():
+#         print(f"[INFO] Image {IMAGE} not found. Building...")
+#         subprocess.run(
+#             ["docker", "build", "-t", IMAGE, "."],
+#             check=True
+#         )
+#     else:
+#         print(f"[INFO] Image {IMAGE} already exists.")
+
+
+
+
+create_folder_and_file()
+delete_folder()
+'''
 
 # --- Step 2: Run container ---
 command = [
@@ -35,3 +60,4 @@ command = [
 
 print(f"[INFO] Running container with {IMAGE}...")
 subprocess.run(command, check=True)
+'''
